053d03a7bc0f23297255123fc77fcf20d520ac53
Michael Ash <mike@mikeash.com>
2013-02-19 20:45:20 -0500

    More comprehensive integer-related tests

--- tester.m
+++ tester.m
@@ -49,15 +49,20 @@ static void Assert(const char *file, int line, const char *name, _Bool cond, NSA
 
 @implementation TestClass
 
+- (id)init
+{
+    if((self = [super init]))
+    {
+        _calledSelectors = [[NSMutableArray alloc] init];
+        _calledArguments = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
 #define RECORD(...) [self addCall: _cmd args: @[ __VA_ARGS__ ]]
 
 - (void)addCall: (SEL)sel args: (NSArray *)args
 {
-    if(!_calledSelectors)
-        _calledSelectors = [[NSMutableArray alloc] init];
-    if(!_calledArguments)
-        _calledArguments = [[NSMutableArray alloc] init];
-    
     [_calledSelectors addObject: NSStringFromSelector(sel)];
     [_calledArguments addObject: args];
 }
@@ -65,6 +70,22 @@ static void Assert(const char *file, int line, const char *name, _Bool cond, NSA
 - (void)empty
 {
     RECORD();
+}
+
+- (void)intArg: (int)x
+{
+    RECORD(@(x));
+}
+
+- (void)lotsOfIntArgs: (int)a : (int)b : (int)c : (int)d : (int)e : (int)f : (int)g : (int)h : (int)i : (int)j
+{
+    RECORD(@(a), @(b), @(c), @(d), @(e), @(f), @(g), @(h), @(i), @(j));
+}
+
+- (int)returnInt
+{
+    RECORD();
+    return 42;
 }
 
 @end
@@ -82,7 +103,53 @@ static void Simple(void)
     ASSERT([obj->_calledArguments isEqual: @[ @[] ]], obj->_calledArguments);
 }
 
+static void Argument(void)
+{
+    TestClass *obj = [[TestClass alloc] init];
+    SEL sel = @selector(intArg:);
+    MAInvocation *inv = [MAInvocation invocationWithMethodSignature: [obj methodSignatureForSelector: sel]];
+    [inv setTarget: obj];
+    [inv setSelector: sel];
+    [inv setArgument: &(int){ 42 } atIndex: 2];
+    [inv invoke];
+    ASSERT([obj->_calledSelectors isEqual: @[ @"intArg:" ]], obj->_calledSelectors);
+    ASSERT([obj->_calledArguments isEqual: @[ @[ @42 ] ]], obj->_calledArguments);
+}
+
+static void LotsOfArguments(void)
+{
+    TestClass *obj = [[TestClass alloc] init];
+    SEL sel = @selector(lotsOfIntArgs::::::::::);
+    MAInvocation *inv = [MAInvocation invocationWithMethodSignature: [obj methodSignatureForSelector: sel]];
+    [inv setTarget: obj];
+    [inv setSelector: sel];
+    for(int i = 0; i < 10; i++)
+        [inv setArgument: &(int){ i } atIndex: i + 2];
+    [inv invoke];
+    ASSERT([obj->_calledSelectors isEqual: @[ @"lotsOfIntArgs::::::::::" ]], obj->_calledSelectors);
+    ASSERT([obj->_calledArguments isEqual: (@[ @[ @0, @1, @2, @3, @4, @5, @6, @7, @8, @9 ] ])], obj->_calledArguments);
+}
+
+static void ReturnValue(void)
+{
+    TestClass *obj = [[TestClass alloc] init];
+    SEL sel = @selector(returnInt);
+    MAInvocation *inv = [MAInvocation invocationWithMethodSignature: [obj methodSignatureForSelector: sel]];
+    [inv setTarget: obj];
+    [inv setSelector: sel];
+    [inv invoke];
+    ASSERT([obj->_calledSelectors isEqual: @[ @"returnInt" ]], obj->_calledSelectors);
+    ASSERT([obj->_calledArguments isEqual: @[ @[] ]], obj->_calledArguments);
+    
+    int val;
+    [inv getReturnValue: &val];
+    ASSERT(val == 42, @(val));
+}
+
 int main(int argc, char **argv)
 {
     TEST(Simple);
+    TEST(Argument);
+    TEST(LotsOfArguments);
+    TEST(ReturnValue);
 }
